483403a670a79586c6c09243a40d4aa2f2683841
johannajonsson <jojo.risin@outlook.com>
2026-05-28 18:50:22 +0200

    update test properties disable aws sqs. update deploy, mock sqs in test

--- src/test/java/se/jensen/johanna/fakestoreinventoryservice/FakeStoreInventoryServiceApplicationTests.java
+++ src/test/java/se/jensen/johanna/fakestoreinventoryservice/FakeStoreInventoryServiceApplicationTests.java
@@ -1,12 +1,21 @@
 package se.jensen.johanna.fakestoreinventoryservice;
 
+import io.awspring.cloud.sqs.operations.SqsTemplate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FakeStoreInventoryServiceApplicationTests {
+
+  @MockitoBean
+  private SqsAsyncClient sqsAsyncClient;
+
+  @MockitoBean
+  private SqsTemplate sqsTemplate;
 
   @Test
   void contextLoads() {
